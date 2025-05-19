599920f0eb1071f94eb8932a638871a5d8d885c2
taryune <taryune@taryu.net>
2025-05-19 13:01:36 +0200

    add models

---
+++ src/atp/src/domain/models/signer.rs
@@ -0,0 +1,72 @@
+use candid::{CandidType, Decode, Encode};
+use ic_stable_structures::{storable::Bound, Storable};
+use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
+
+#[derive(CandidType, Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub enum SignatureAlgorithm {
+    #[serde(rename = "ecdsa")]
+    Ecdsa,
+    #[serde(rename = "schnorr")]
+    Schnorr,
+}
+
+impl Storable for SignatureAlgorithm {
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), SignatureAlgorithm).unwrap()
+    }
+
+    const BOUND: Bound = Bound::Unbounded;
+}
+
+#[derive(CandidType, Clone, Serialize, Deserialize, Debug, PartialEq)]
+pub enum Curve {
+    #[serde(rename = "secp256k1")]
+    Secp256k1,
+    #[serde(rename = "ed25519")]
+    Ed25519,
+}
+impl Storable for Curve {
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Curve).unwrap()
+    }
+
+    const BOUND: Bound = Bound::Unbounded;
+}
+
+pub struct PublicKey {
+    pub sec1: Vec<u8>,
+    pub x: Vec<u8>,
+    pub y: Option<Vec<u8>>,
+}
+
+pub struct ThresholdSigner {
+    algorithm: SignatureAlgorithm,
+    curve: Curve,
+    key_name: String,
+    derivation_path: String,
+}
+
+impl ThresholdSigner {
+    pub fn new(
+        algorithm: SignatureAlgorithm,
+        curve: Curve,
+        key_name: String,
+        derivation_path: String,
+    ) -> Self {
+        ThresholdSigner {
+            algorithm,
+            curve,
+            key_name,
+            derivation_path,
+        }
+    }
+}
