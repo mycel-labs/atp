de9ade9e02c34b7b36fff69fec20858c0fc01ed6
taryune <taryune@taryu.net>
2025-05-20 08:47:45 +0200

    fmt

--- src/atp/src/utils/config.rs
+++ src/atp/src/utils/config.rs
@@ -3,6 +3,7 @@
 * test_key_1: Test key available on the ICP mainnet.
 * key_1: Production key available on the ICP mainnet.
 */
+
 pub const KEY_ID: &str = "dfx_test_key";
 // pub const KEY_ID: &str = "test_key_1";
 // pub const KEY_ID: &str = "key_1";
