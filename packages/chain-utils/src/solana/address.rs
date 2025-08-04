eb4695f17b00596c8e8464010c92e8160a18c194
taryune <taryune@taryu.net>
2025-08-04 14:42:22 +0900

    feat: generate solana address

--- packages/chain-utils/src/solana/address.rs
+++ packages/chain-utils/src/solana/address.rs
@@ -1,2 +1,11 @@
-
-
+pub fn generate_address(pub_key_hex_string: String) -> Result<String, String> {
+    let pub_key_bytes = hex::decode(&pub_key_hex_string)
+        .map_err(|e| format!("Failed to decode public key hex: {}", e))?;
+    // Ensure the public key is 32 bytes long
+    if pub_key_bytes.len() != 32 {
+        return Err("Public key must be 32 bytes long".to_string());
+    }
+    // Encode the public key to a base58 string
+    let pub_key_base58_string = bs58::encode(pub_key_bytes).into_string();
+    Ok(pub_key_base58_string)
+}
